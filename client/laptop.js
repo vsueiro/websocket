19d1a77dab0d7824af57daec0eec08af7b493098
Vinicius Sueiro <hi@vsueiro.com>
2022-11-15 02:32:20 -0500

    Update server

--- client/laptop.js
+++ client/laptop.js
@@ -2,7 +2,9 @@
 // let socket = io('ws://localhost:3000');
 
 // This did (found at https://stackoverflow.com/q/44628363)
-let socket = io('http://localhost:3000', { transports : ['websocket'] });
+// let socket = io('http://localhost:8080', { transports : ['websocket'] });
+
+let socket = io('https://server.supertunnel.app/', { transports : ['websocket'] });
 
 socket.on('message', show);
 
